59234d243810b18fd14e6241b3f15d851053f046
Your Name <you@example.com>
2019-03-23 15:43:04 +0530

    modularize ccode

--- test101.py
+++ test101.py
@@ -1,3 +1,7 @@
-for i in range(2000,3201):
-    if(i%7==0 and i%5!=0):
-        print(i)
+def getDiviser(x,y):
+    for i in range(x,y+1):
+        if(i%7==0 and i%5!=0):
+            print(i)
+
+if __name__ == "__main__":
+    getDiviser(2000,3200)    
