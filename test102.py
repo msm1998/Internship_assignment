8157a1aa7b7f187db61c5297d6df9c09d52161c9
Your Name <you@example.com>
2019-03-23 16:24:09 +0530

    changes added

--- test102.py
+++ test102.py
@@ -1,17 +1,19 @@
 from multiprocessing import Process,Queue
 import random
 def getRandomNumber(q):
-    q.put(random.randint(0,10))
+    randNum = random.randint(0,10)
+    q.put(randNum)
+    print(randNum)
 
 def writeRandomNumber(q):
     while not q.empty():
-        print(q.get());
+        print(q.get())
 
 if __name__ == "__main__":
     q = Queue()
     p1 = Process(target = getRandomNumber,args=(q,))
     p2 = Process(target = writeRandomNumber,args=(q,))
     p1.start()
-    p2.start()
     p1.join()
+    p2.start()
     p2.join()
