4da4a22fa203b8da94f10d368825d2eab53a7d6c
Your Name <you@example.com>
2019-03-23 15:50:29 +0530

    Revert "modularize ccode"
    This reverts commit 59234d243810b18fd14e6241b3f15d851053f046.

--- test101.py
+++ test101.py
@@ -1,7 +1,3 @@
-def getDiviser(x,y):
-    for i in range(x,y+1):
-        if(i%7==0 and i%5!=0):
-            print(i)
-
-if __name__ == "__main__":
-    getDiviser(2000,3200)    
+for i in range(2000,3201):
+    if(i%7==0 and i%5!=0):
+        print(i)
